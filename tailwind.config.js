fad6329cde03349599028b5fb8309de1f9c08f8b
Jonath-z <jonathanzihindula95@gmail.com>
2022-10-19 17:00:37 +0200

    feat: stripe clone desk version

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "primary",
+        secondary: "secondary",
+      },
+      colors: {
+        "semi-gray": "#0a2540",
+        "stripe-blue": "#635bff",
+        "stripe-sky-blue": "#00d4ff",
+        "stripe-deco-blue": "#02bcf5",
+        "stripe-semi-violet": "#7a73ff",
+      },
+    },
+  },
+  plugins: [],
+};
